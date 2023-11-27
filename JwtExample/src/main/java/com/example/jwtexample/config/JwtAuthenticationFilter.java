182dce681197439cc67994d50ec26cadfb8ddb4b
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-11-27 17:54:34 +0200

    Uploading project

--- JwtExample/src/main/java/com/example/jwtexample/config/JwtAuthenticationFilter.java
+++ JwtExample/src/main/java/com/example/jwtexample/config/JwtAuthenticationFilter.java
@@ -43,9 +43,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                         null,
                         userDetails.getAuthorities());
 
-                authToken.setDetails(
-                        new WebAuthenticationDetailsSource().buildDetails(request)
-                );
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
